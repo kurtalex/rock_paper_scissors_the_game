32b44225f157f53759392daf6520cb2f92e360b9
Alexander Kekh <kurt_alex@mail.ru>
2019-10-12 19:20:20 +0300

    Create client logic

--- network.py
+++ network.py
@@ -5,8 +5,8 @@ import pickle
 class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server = "192.168.0.26"
-        self.port = 5555
+        self.server = "ENTER VALID IP ADDRESS"  # For example 192.168.0.1
+        self.port = 5555  # If you want you may change this port
         self.addr = (self.server, self.port)
         self.p = self.connect()
 
